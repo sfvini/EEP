ff456a214c6bfc0cebdb9c4e3a7a260a2ec80801
Vinícius França <vinicius.sf2007@aluno.ifsc.edu.br>
2026-07-24 10:58:08 -0300

    Implement Q18 function for defect probability calculation
    Add initial implementation of Q18 function to calculate defect probability.

---
+++ Q18.py
@@ -0,0 +1,10 @@
+import r in random
+'''
+Uma fábrica tem três máquinas que fixam parafusos. O percentual de operações feitas por cada
+máquina em relação ao total é de 10%, 35% e 55%, respectivamente. Além disso, sabe-se que 5%, 
+3% e 1% das operações das respectivas três máquinas apresentam defeito. Qual é a probabilidade 
+de que um parafuso selecionado aleatoriamente no final das execuções de produção do dia estar com falha?
+'''
+def Q18():
+  Iteracoes = int(1e6)
+  Sucessos = 0

a9fb5069324b301d0079511844e5f45fa27c09e9
Vinícius França <vinicius.sf2007@aluno.ifsc.edu.br>
2026-07-24 11:06:12 -0300

    Refactor probability calculation in Q18.py

--- Q18.py
+++ Q18.py
@@ -6,10 +6,21 @@ máquina em relação ao total é de 10%, 35% e 55%, respectivamente. Além diss
 de que um parafuso selecionado aleatoriamente no final das execuções de produção do dia estar com falha?
 '''
 def Q18():
-  Iteracoes = int(1e6)
-  Sucessos = 0
-
-
+    Iteracoes = int(1e6)
+    Sucessos = 0
+    
+    for _ in range(Iteracoes):
+        maquina = r.random()
+        
+        if maquina < 0.10:
+            if r.random() < 0.05:
+                Sucessos += 1
+        elif maquina < 0.45: 
+            if r.random() < 0.03:
+                Sucessos += 1
+        else:
+            if r.random() < 0.01:
+                Sucessos += 1
 			
 	print(Sucessos / Iteracoes)
 		

6ab7059c540b757740fd897a8117dfb88b407d94
Vinícius França <vinicius.sf2007@aluno.ifsc.edu.br>
2026-08-05 16:05:18 -0300

    Create Q42.py

---
+++ T1/Q42.py
@@ -0,0 +1,16 @@
+import random as r
+'''
+Um jogo de computador requer que um jogador encontre abrigo seguro em um local onde seus inimigos não podem entrar. Quatro portas aparecem para o jogador,
+a partir do qual ele deve escolher entre uma e apenas uma. O jogador deve então fazer uma segunda escolha entre dois, quatro, um ou cinco buracos para descer, 
+respectivamente, dependendo de qual porta ela atravessa. Em cada caso, apenas um buraco leva ao refúgio seguro. O jogador é apressado em tomar uma
+decisão e em sua pressa faz escolhas aleatoriamente. Qual é a probabilidade de ter chegando com segurança ao refúgio?
+'''
+def Q42():
+    Iteracoes = int(1e6)
+    Sucessos = 0
+
+	print(Sucessos / Iteracoes)
+		
+if __name__ == "__main__":
+	Q42()
+	pass

375cdc2d64e5e7791e1527ae58ae2133f4ea2391
Vinícius França <vinicius.sf2007@aluno.ifsc.edu.br>
2026-08-06 18:28:37 -0300

    Update Q18.py

--- T1/Q18.py
+++ T1/Q18.py
@@ -1,6 +1,4 @@
-import random as r
 import math
-
 '''
 De quantas formas é possível dividir 20 pessoas:
 (a) em dois grupos de 10.
@@ -8,19 +6,25 @@ De quantas formas é possível dividir 20 pessoas:
 (c) em três grupos de 6 e um de 2.
 '''
 def Q18():
-    pessoas = 20
-	a = 0
-	b = 0
-	c = 0
-                
-	a = (math.factorial(20) / math.factorial(10) * math.factorial(10)) * 1
-    b = (math.factorial(20) / math.factorial(5) * math.factorial(15)) * (math.factorial(15) / math.factorial(5) * math.factorial(10)) * (math.factorial(10) / math.factorial(5) * math.factorial(5)) * 1
-    c = (math.factorial(20) / math.factorial(6) * math.factorial(14)) * (math.factorial(14) / math.factorial(6) * math.factorial(8)) * (math.factorial(8) / math.factorial(6) * math.factorial(2)) * 1
-			
-	print(a)
-	print(b)
-	print(c)
-		
+    a = (
+        math.factorial(20) / (math.factorial(10) * math.factorial(10))
+    ) / math.factorial(2)
+
+    b = (
+        (math.factorial(20) / (math.factorial(5) * math.factorial(15)))
+        * (math.factorial(15) / (math.factorial(5) * math.factorial(10)))
+        * (math.factorial(10) / (math.factorial(5) * math.factorial(5)))
+    ) / math.factorial(4)
+
+    c = (
+        (math.factorial(20) / (math.factorial(6) * math.factorial(14)))
+        * (math.factorial(14) / (math.factorial(6) * math.factorial(8)))
+        * (math.factorial(8) / (math.factorial(6) * math.factorial(2)))
+    ) / math.factorial(3)
+
+    print(a)
+    print(b)
+    print(c)
+
 if __name__ == "__main__":
-	Q18()
-	pass
+    Q18()

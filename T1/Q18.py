84cadc0373292d24e05425fc0305c9577a058bdf
Vinícius França <vinicius.sf2007@aluno.ifsc.edu.br>
2026-08-06 18:53:34 -0300

    Simplify calculations in Q18 function
    Removed unnecessary divisions by factorials for b and c calculations.

--- T1/Q18.py
+++ T1/Q18.py
@@ -6,21 +6,9 @@ De quantas formas é possível dividir 20 pessoas:
 (c) em três grupos de 6 e um de 2.
 '''
 def Q18():
-    a = (
-        math.factorial(20) / (math.factorial(10) * math.factorial(10))
-    ) / math.factorial(2)
-
-    b = (
-        (math.factorial(20) / (math.factorial(5) * math.factorial(15)))
-        * (math.factorial(15) / (math.factorial(5) * math.factorial(10)))
-        * (math.factorial(10) / (math.factorial(5) * math.factorial(5)))
-    ) / math.factorial(4)
-
-    c = (
-        (math.factorial(20) / (math.factorial(6) * math.factorial(14)))
-        * (math.factorial(14) / (math.factorial(6) * math.factorial(8)))
-        * (math.factorial(8) / (math.factorial(6) * math.factorial(2)))
-    ) / math.factorial(3)
+    a =  math.factorial(20) / (math.factorial(10) * math.factorial(10))
+    b =  (math.factorial(20) / (math.factorial(5) * math.factorial(15))) * (math.factorial(15) / (math.factorial(5) * math.factorial(10))) * (math.factorial(10) / (math.factorial(5) * math.factorial(5)))
+    c =  (math.factorial(20) / (math.factorial(6) * math.factorial(14))) * (math.factorial(14) / (math.factorial(6) * math.factorial(8))) * (math.factorial(8) / (math.factorial(6) * math.factorial(2)))
 
     print(a)
     print(b)

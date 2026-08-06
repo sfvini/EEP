9114830df799ace0bb6b4f7d2b8e273084362398
Vinícius França <vinicius.sf2007@aluno.ifsc.edu.br>
2026-08-05 23:14:50 -0300

    Implement game logic for safe refuge probability

--- T1/Q42.py
+++ T1/Q42.py
@@ -1,4 +1,5 @@
 import random as r
+
 '''
 Um jogo de computador requer que um jogador encontre abrigo seguro em um local onde seus inimigos não podem entrar. Quatro portas aparecem para o jogador,
 a partir do qual ele deve escolher entre uma e apenas uma. O jogador deve então fazer uma segunda escolha entre dois, quatro, um ou cinco buracos para descer, 
@@ -6,11 +7,25 @@ respectivamente, dependendo de qual porta ela atravessa. Em cada caso, apenas um
 decisão e em sua pressa faz escolhas aleatoriamente. Qual é a probabilidade de ter chegando com segurança ao refúgio?
 '''
 def Q42():
-    Iteracoes = int(1e6)
-    Sucessos = 0
+  Iteracoes = int(1e6)
+  Sucessos = 0
 
-	print(Sucessos / Iteracoes)
-		
-if __name__ == "__main__":
-	Q42()
-	pass
+  for i in range(Iteracoes):
+    porta = r.randint(1, 4)
+
+    if porta == 1:
+      buracos = 2
+    elif porta == 2:
+      buracos = 4
+    elif porta == 3:
+      buracos = 1
+    else:
+      buracos = 5
+
+    if r.randint(1, buracos) == 1:
+      Sucessos = Sucessos + 1
+
+  print(Sucessos / Iteracoes)
+
+if __name__ == '__main__':
+  Q42()

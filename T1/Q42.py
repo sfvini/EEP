3759b8e9e64272dfc65072276344e5b78c6cc108
Vinícius França <vinicius.sf2007@aluno.ifsc.edu.br>
2026-08-06 18:49:59 -0300

    Refactor Q42 function for worker selection probability
    Refactor the Q42 function to calculate combinations for a group of workers instead of simulating a game. Remove random choices and replace with mathematical calculations.

--- T1/Q42.py
+++ T1/Q42.py
@@ -1,31 +1,18 @@
-import random as r
+import math
+'''
+Numa fábrica com 10 trabalhadores, um grupo de 4 será selecionado para uma excursão, 
+que será sorteada pela empresa. De quantas maneiras o grupo poderá ser formado se 
+dois dos dez são casados e só irão juntos?
+'''
 
-'''
-Um jogo de computador requer que um jogador encontre abrigo seguro em um local onde seus inimigos não podem entrar. Quatro portas aparecem para o jogador,
-a partir do qual ele deve escolher entre uma e apenas uma. O jogador deve então fazer uma segunda escolha entre dois, quatro, um ou cinco buracos para descer, 
-respectivamente, dependendo de qual porta ela atravessa. Em cada caso, apenas um buraco leva ao refúgio seguro. O jogador é apressado em tomar uma
-decisão e em sua pressa faz escolhas aleatoriamente. Qual é a probabilidade de ter chegando com segurança ao refúgio?
-'''
 def Q42():
-  Iteracoes = int(1e6)
-  Sucessos = 0
-
-  for i in range(Iteracoes):
-    porta = r.randint(1, 4)
-
-    if porta == 1:
-      buracos = 2
-    elif porta == 2:
-      buracos = 4
-    elif porta == 3:
-      buracos = 1
-    else:
-      buracos = 5
-
-    if r.randint(1, buracos) == 1:
-      Sucessos = Sucessos + 1
-
-  print(Sucessos / Iteracoes)
+    # Sem casal
+    res1 = math.factorial(8) / (math.factorial(4) * math.factorial(4))
+    print(res1)
+                               
+    # Com casal
+    res2 = math.factorial(8) / (math.factorial(2) * math.factorial(6))
+    print(res2)
 
 if __name__ == '__main__':
-  Q42()
+    Q42()

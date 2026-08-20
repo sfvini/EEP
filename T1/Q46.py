c560ceaff1030006a2c3e0b306526c8c4d9d5210
Vinícius França <viniciusicmsc@gmail.com>
2026-08-20 08:57:30 -0300

    Atualizar o Q46.py

--- T1/Q46.py
+++ T1/Q46.py
@@ -4,6 +4,12 @@ Quantos planos são determinados por quatro pontos distintos e não coplanares?
 '''
 
 def Q46():
+
+'''
+EXPLICAÇÃO DA IA
+​Como o enunciado afirma que temos 4 pontos "não coplanares" (ou seja, eles não estão todos achatados no mesmo plano; formam uma figura 3D, como as pontas de uma pirâmide de base triangular chamada tetraedro), qualquer trio de pontos que você escolher formará um plano garantido.
+'''
+
     res = math.factorial(4) / (math.factorial(3) * math.factorial(1))
     print(res)
 
